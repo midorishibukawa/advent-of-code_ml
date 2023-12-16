269dd12476a0dfbefd0a59bceddb521cf9bd0e03
midorishibukawa <midori@shibukawa.io>
2023-12-16 19:34:18 -0300

    solve day 06 bonus challenge

--- lib/y23/test/day06.ml
+++ lib/y23/test/day06.ml
@@ -1,4 +1,5 @@
 let solve = Y23.solve 6 ~bonus:false;;
+let solve_bonus = Y23.solve 6 ~bonus:true;;
 
 let example =
 "Time:      7  15   30
@@ -13,3 +14,7 @@ let%test "two games" = solve "Time: 2 3\nDistance: 0 1" = 2;;
 let%test "three games" = solve "Time: 2 3 4\nDistance: 0 1 2" = 6;;
 
 let%test "example" = solve example = 288;;
+
+let%test "two fake games" = solve_bonus "Time: 2 3\n Distance: 0 1" = 22;;
+
+let%test "example bonus" = solve_bonus example = 71503;;
