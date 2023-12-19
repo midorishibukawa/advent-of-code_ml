46aa0767a45c22cb2842e5ca82d88eedb84f3b66
midorishibukawa <midori@shibukawa.io>
2023-12-19 16:24:14 -0300

    solve day 07 bonus challenge

--- lib/y23/day07.ml
+++ lib/y23/day07.ml
@@ -27,22 +27,29 @@ let val_of_kind hand_type =
     | One_pair          -> 1
     | High_card         -> 0;;
 
-let get_kind_from_map map =
+let get_kind_from_map ~bonus map =
+    let (j, map) = 
+        if not bonus 
+        then (0, map)
+        else 
+            try IntMap.extract 1 map 
+            with _ -> (0, map) in
     let vals = List.of_enum @@ IntMap.values map in
-    let ls = List.length vals in
+    let ls = List.length vals in 
     match ls with 
     | 1 -> Five_of_a_kind 
-    | 2 -> if List.max vals = 4 then Four_of_a_kind else Full_house 
-    | 3 -> if List.max vals = 3 then Three_of_a_kind else Two_pair 
+    | 2 -> if List.max vals + j = 4 then Four_of_a_kind else Full_house 
+    | 3 -> if List.max vals + j = 3 then Three_of_a_kind else Two_pair 
     | 4 -> One_pair 
+    | 0 -> if j > 0 then Five_of_a_kind else High_card
     | _ -> High_card;;
 
-let get_val card =
+let get_val ~bonus card =
     match Char.uppercase_ascii card with 
     | 'A' -> 14
     | 'K' -> 13
     | 'Q' -> 12
-    | 'J' -> 11
+    | 'J' -> if bonus then 1 else 11
     | 'T' -> 10
     | c -> if Char.is_digit c then to_digit c else -1;;
 
@@ -61,10 +68,11 @@ let sort_hands a b =
     then rdif 
     else sort_hands_by_cards a.cs b.cs;;
 
-let rec get_hand ?(cs=[]) ?(map=IntMap.empty) ?(bid=0) ?(phase=Hand) xs =
+let rec get_hand ?(cs=[]) ?(map=IntMap.empty) ?(bid=0) ?(phase=Hand) ~bonus xs =
+    let get_hand = get_hand ~bonus in
     let card () =
         { cs = List.rev cs
-        ; kind = get_kind_from_map map 
+        ; kind = get_kind_from_map ~bonus map 
         ; bid = bid } in
     match xs with 
     | [] -> (card (), [])
@@ -73,7 +81,7 @@ let rec get_hand ?(cs=[]) ?(map=IntMap.empty) ?(bid=0) ?(phase=Hand) xs =
     | (x::xs') ->
             match phase with
             | Hand -> 
-                    let c = get_val x in
+                    let c = get_val ~bonus x in
                     let map = IntMap.modify_def 0 c (Stdlib.(+) 1) map in
                     get_hand ~cs:(c::cs) ~map xs' 
             | Bid -> get_hand ~cs ~map ~bid:(bid * 10 + to_digit x) ~phase xs'
@@ -83,7 +91,7 @@ let rec solve ?(hs=[]) ~bonus xs =
     match xs with
     | [] -> let hs = List.sort sort_hands hs in
             List.fold_lefti (fun acc i h -> acc + (i + 1) * h.bid) 0 hs
-    | xs -> let (h, xs') = get_hand xs in 
+    | xs -> let (h, xs') = get_hand ~bonus xs in
             solve ~hs:(h::hs) xs'
     
 
--- lib/y23/test/day07.ml
+++ lib/y23/test/day07.ml
@@ -1,4 +1,5 @@
 let solve = Y23.solve 7 ~bonus:false;;
+let solve_bonus = Y23.solve 7 ~bonus:true;;
 
 let example =
 "32T3K 765
@@ -8,9 +9,13 @@ KTJJT 220
 QQQJA 483";;
 
 let%test "one hand" = solve "AAAAA 1" = 1;;
-
 let%test "two dif kind hands" = solve "AAAAA 3\nAAAAK 5" = 11;;
-
 let%test "two same kind hands" = solve "AAAAK 3\nKAAAA 5" = 11;;
-
 let%test "example" = solve example = 6440;;
+let%test "one hand bonus" = solve_bonus "AAAAA 1" = 1;;
+let%test "two dif kind hands bonus" = solve_bonus "AAAAA 3\nJJJJK 5" = 11;;
+
+let%test "test joker" = solve_bonus "AAAAK 3\nJJJJK 5" = 13;;
+let%test "test joker 2" = solve_bonus "AAAKK 3\nJJKK2 5" = 13;;
+let%test "all jokers bonus" = solve_bonus "JJJJJ 2\nKKKKA 3\nAAAJJ 5" = 22;;
+let%test "example bonus" = solve_bonus example = 5905;;
